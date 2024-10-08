f3f5d8eb131d11b26746e40575ebe8e4cad8f9cc
Aziz-khan88 <aziz.rehman@crystallitedigital.com>
2024-10-08 20:27:30 +0500

    chagnes

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -3,7 +3,7 @@ import Faqs from "@/src/app/home/faq";
 import Industries from "@/src/app/home/industries";
 import Testimonials from "@/src/app/home/testimonials";
 import Banner from "@/src/app/portfolio/components/banner";
-import PortfolioTabs from "@/src/app/portfolio/components/PortfolioTabs";
+import PortfolioTabs from "@/src/app/portfolio/components/portfoliotabs";
 
 import Test01 from "media/home/testimonial/test01.webp"
 import Test02 from "media/home/testimonial/test02.webp"
